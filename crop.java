30eb6e3c249755ba0b5f4ef7818c2b7d294f1116
ananysharma04 <145222211+ananysharma04@users.noreply.github.com>
2023-10-12 23:00:57 +0530

    Update crop.java
    Adding some new things in the repository

--- crop.java
+++ crop.java
@@ -1,13 +1,9 @@
-import java.util.ArrayList;
-import java.util.List;
 
-// Interface for any item that can be bought
 interface Purchasable {
     String getName();
     double getPrice();
 }
 
-// Class representing a Crop
 class Crop implements Purchasable {
     private String name;
     private double pricePerKg;
@@ -45,43 +41,35 @@ class Farmer extends Person {
     }
 }
 
-// Class representing a Consumer
 class Consumer extends Person {
-    private List<Purchasable> shoppingCart;
 
     public Consumer(String name) {
-        super(name);
-        shoppingCart = new ArrayList<>();
+    
+    
     }
 
-    public void addToCart(Purchasable item) {
-        shoppingCart.add(item);
-    }
+   
 
-    public void checkout() {
+    public void paymentDeatails() {
         double totalAmount = 0;
         System.out.println("Receipt for " + getName() + ":");
-        for (Purchasable item : shoppingCart) {
-            totalAmount += item.getPrice();
-            System.out.println(item.getName() + ": $" + item.getPrice());
-        }
+      
         System.out.println("Total: $" + totalAmount);
     }
 }
 
 public class CropBuyingSystem {
     public static void main(String[] args) {
-        Purchasable wheat = new Crop("Wheat", 2.5);
-        Purchasable corn = new Crop("Corn", 3.0);
+        Purchasable wheat = new Crop();
+        Purchasable corn = new Crop();
 
-        Farmer farmer1 = new Farmer("John");
-        Farmer farmer2 = new Farmer("Alice");
+        Farmer farmer1 = new Farmer();
+        Farmer farmer2 = new Farmer();
 
-        Consumer consumer = new Consumer("Bob");
+        Consumer consumer = new Consumer();
 
-        consumer.addToCart(wheat);
-        consumer.addToCart(corn);
+        
 
-        consumer.checkout();
+       
     }
 }
